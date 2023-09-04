fa50a1c6f02e072c2211580a06e187ff1d0a7fa8
asma <asma.shaikh@solvezy.com>
2023-09-04 15:31:53 +0530

    log4j2 Implementation

--- src/main/java/com/solv/inventory/service/UserServiceImpl.java
+++ src/main/java/com/solv/inventory/service/UserServiceImpl.java
@@ -1,11 +1,18 @@
 package com.solv.inventory.service;
 
+import com.solv.inventory.config.Log4j2Config;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
+
+import static com.solv.inventory.config.Log4j2Config.getLoggerVariable;
+import static com.solv.inventory.config.Log4j2Config.logger;
 
 @Service
 public class UserServiceImpl implements UserService{
+    private static final Logger serviceLogger = getLoggerVariable("UserServiceImpl");
     @Override
     public String saveUser(String userName) {
+        logger.info("Create User Request : "+ userName);
         return userName + " User Saved Successfully";
     }
 }
